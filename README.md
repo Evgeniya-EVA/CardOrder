13b3eae72e40d8c10bb02924214ceb2ea5131508
Evgeniya-EVA <evgeniya.anisimova.eva@gmail.com>
2019-12-18 17:56:01 +0300

    feat: commit for another workstation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Сервис заявки на дебетовую карту
+
+[![Build status](https://ci.appveyor.com/api/projects/status/3n2e4f2g5mkg9trj?svg=true)](https://ci.appveyor.com/project/Evgeniya-EVA/cardorder)
---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardOrderTest {
+
+    static WebDriver driver = new ChromeDriver();
+
+    @Test
+    static void inputFieldsTest() throws InterruptedException {
+        driver.get("http://localhost:9999");
+       // List<WebElement> form = driver.findElements(By.cssSelector("[data_test_id=input__control]"));
+        driver.findElement(By.cssSelector("[data_test_id=name]")).findElement(By.className("input__control")).sendKeys("Иванов Василий");
+        driver.findElement(By.cssSelector("[data_test_id=phone]")).findElement(By.className("input__control")).sendKeys("+79250000000");
+        driver.findElement(By.cssSelector("[data_test_id=agreement]")).findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data_test_id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        Thread.sleep(5000);
+
+
+
+    }
+}
