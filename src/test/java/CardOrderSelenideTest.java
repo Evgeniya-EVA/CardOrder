4104c1ff4bd7012936a2dda02da154695bf5f0b9
Evgeniya-EVA <evgeniya.anisimova.eva@gmail.com>
2019-12-27 16:48:52 +0300

    fix: access modifiers added, HashMap variable instead of ArrayList

--- src/test/java/CardOrderSelenideTest.java
+++ src/test/java/CardOrderSelenideTest.java
@@ -6,24 +6,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderSelenideTest {
 
-    List<SelenideElement> elementList = new ArrayList<>();
-    String warningColor = "rgba(255, 92, 92, 1)";
+    private Map<String, SelenideElement> elementList = new HashMap<>();
+    private String warningColor = "rgba(255, 92, 92, 1)";
 
     @BeforeEach
     void setUp() {
         open("http://localhost:9999");
-        elementList.add($("[data-test-id=\"name\"]"));
-        elementList.add($("[data-test-id=\"phone\"]"));
-        elementList.add($("[data-test-id=\"agreement\"]"));
-        elementList.add($("button"));
-        elementList.add($("[data-test-id=\"order-success\"]"));
+        elementList.put("name", $("[data-test-id=\"name\"]"));
+        elementList.put("phone", $("[data-test-id=\"phone\"]"));
+        elementList.put("agreement", $("[data-test-id=\"agreement\"]"));
+        elementList.put("button", $("button"));
+        elementList.put("success", $("[data-test-id=\"order-success\"]"));
     }
 
     @AfterEach
@@ -35,71 +36,71 @@ public class CardOrderSelenideTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/cardOrderValidInputData.csv", numLinesToSkip = 1, encoding = "UTF-8")
     void inputFormSelenideTest(String name, String phone){
-       elementList.get(0).$(By.name("name")).setValue(name);
-       elementList.get(1).$(By.name("phone")).setValue(phone);
-       elementList.get(2).click();
-       elementList.get(3).click();
-       elementList.get(4).shouldHave(Condition.exactText(
+       elementList.get("name").$(By.name("name")).setValue(name);
+       elementList.get("phone").$(By.name("phone")).setValue(phone);
+       elementList.get("agreement").click();
+       elementList.get("button").click();
+       elementList.get("success").shouldHave(Condition.exactText(
                "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void inputNameShouldBeFilledTest(){
-        elementList.get(0).$(By.name("name")).setValue("");
-        elementList.get(1).$(By.name("phone")).setValue("+79250000000");
-        elementList.get(2).click();
-        elementList.get(3).click();
-        elementList.get(0).$(By.className("input__sub"))
+        elementList.get("name").$(By.name("name")).setValue("");
+        elementList.get("phone").$(By.name("phone")).setValue("+79250000000");
+        elementList.get("agreement").click();
+        elementList.get("button").click();
+        elementList.get("name").$(By.className("input__sub"))
                 .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void inputNameInvalidInputSubTextShouldBeRedTest(){
-        elementList.get(0).$(By.name("name")).setValue("");
-        elementList.get(1).$(By.name("phone")).setValue("+79250000000");
-        elementList.get(2).click();
-        elementList.get(3).click();
-        elementList.get(0).$(By.className("input__sub"))
+        elementList.get("name").$(By.name("name")).setValue("");
+        elementList.get("phone").$(By.name("phone")).setValue("+79250000000");
+        elementList.get("agreement").click();
+        elementList.get("button").click();
+        elementList.get("name").$(By.className("input__sub"))
                 .shouldBe(Condition.cssValue("color", warningColor));
     }
 
     @Test
     void inputPhoneShouldBeFilledTest(){
-        elementList.get(0).$(By.name("name")).setValue("Иван Иванов");
-        elementList.get(1).$(By.name("phone")).setValue("");
-        elementList.get(2).click();
-        elementList.get(3).click();
-        elementList.get(1).$(By.className("input__sub"))
+        elementList.get("name").$(By.name("name")).setValue("Иван Иванов");
+        elementList.get("phone").$(By.name("phone")).setValue("");
+        elementList.get("agreement").click();
+        elementList.get("button").click();
+        elementList.get("phone").$(By.className("input__sub"))
                 .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void inputPhoneInvalidInputSubTextShouldBeRedTest(){
-        elementList.get(0).$(By.name("name")).setValue("Иван Иванов");
-        elementList.get(1).$(By.name("phone")).setValue("");
-        elementList.get(2).click();
-        elementList.get(3).click();
-        elementList.get(1).$(By.className("input__sub"))
+        elementList.get("name").$(By.name("name")).setValue("Иван Иванов");
+        elementList.get("phone").$(By.name("phone")).setValue("");
+        elementList.get("agreement").click();
+        elementList.get("button").click();
+        elementList.get("phone").$(By.className("input__sub"))
                 .shouldBe(Condition.cssValue("color", warningColor));
     }
 
     @Test
     void checkBoxShouldBeFilledTest(){
-        elementList.get(0).$(By.name("name")).setValue("Иванов Иван");
-        elementList.get(1).$(By.name("phone")).setValue("+79250000000");
-        elementList.get(3).click();
-        elementList.get(2).$(By.className("checkbox__text"))
+        elementList.get("name").$(By.name("name")).setValue("Иванов Иван");
+        elementList.get("phone").$(By.name("phone")).setValue("+79250000000");
+        elementList.get("button").click();
+        elementList.get("agreement").$(By.className("checkbox__text"))
                 .shouldBe(Condition.cssValue("color", warningColor));
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/cardOrderInvalidNameInputData.csv", numLinesToSkip = 1, encoding = "UTF-8")
     void inputNameInvalidDataWarningTest(String name, String phone){
-        elementList.get(0).$(By.name("name")).setValue(name);
-        elementList.get(1).$(By.name("phone")).setValue(phone);
-        elementList.get(2).click();
-        elementList.get(3).click();
-        elementList.get(0).$(By.className("input__sub"))
+        elementList.get("name").$(By.name("name")).setValue(name);
+        elementList.get("phone").$(By.name("phone")).setValue(phone);
+        elementList.get("agreement").click();
+        elementList.get("button").click();
+        elementList.get("name").$(By.className("input__sub"))
                 .shouldHave(Condition.exactText(
                         "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
@@ -107,11 +108,11 @@ public class CardOrderSelenideTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/cardOrderInvalidPhoneInputData.csv", numLinesToSkip = 1, encoding = "UTF-8")
     void inputPhoneInvalidDataWarningTest(String name, String phone){
-        elementList.get(0).$(By.name("name")).setValue(name);
-        elementList.get(1).$(By.name("phone")).setValue(phone);
-        elementList.get(2).click();
-        elementList.get(3).click();
-        elementList.get(1).$(By.className("input__sub"))
+        elementList.get("name").$(By.name("name")).setValue(name);
+        elementList.get("phone").$(By.name("phone")).setValue(phone);
+        elementList.get("agreement").click();
+        elementList.get("button").click();
+        elementList.get("phone").$(By.className("input__sub"))
                 .shouldHave(
                         Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
